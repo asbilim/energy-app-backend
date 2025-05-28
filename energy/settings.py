26a64c018f7791493d01987545b35ffb8dc9dd32
unknown <bilimdepaullilian@gmail.com>
2025-05-28 21:35:22 +0100

    going to prod

--- energy/settings.py
+++ energy/settings.py
@@ -79,24 +79,27 @@ WSGI_APPLICATION = 'energy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# For production, use PostgreSQL (commented out for now)
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# For production, use PostgreSQL (commented out for now)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'energy-db',
+        'USER': 'energy-db_owner',
+        'PASSWORD': 'npg_kXCqE6ZbK7xz',
+        'HOST': 'ep-shiny-bonus-a8bm1py6-pooler.eastus2.azure.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
+}
 
 
 # Password validation
