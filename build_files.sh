07e7785dba21b75a83c74fd21098b412c83d9f7e
unknown <bilimdepaullilian@gmail.com>
2025-05-28 22:20:16 +0100

    dsd

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Install Node.js dependencies (including Tailwind CSS)
+npm install
+
+# Compile Tailwind CSS (using the script from package.json)
+npm run build:css
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Collect Django static files
 python manage.py collectstatic --noinput
